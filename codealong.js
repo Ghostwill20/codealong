d3b201ae915b17d0df801e5b7475a3bb67b34b36
Ghostwill20 <carwilli21@gmail.com>
2020-02-04 12:54:41 -0500

    fix mistakes

--- codealong.js
+++ codealong.js
@@ -1,17 +1,19 @@
 function printPrimes (){
-    for (var i = 0; i < 1000; i++){
+    for(var i = 0; i < 1000; i++){
         if(i >= 0 && i <= 3){
         console.log(i); 
          } else if(i % 2 !=0 || i % 3 !=0){
            //this number less then 3 is odd and isnt divisble by 3
             var j = 5;
             while(j*j <= i){
-                if(i*j ===0 || i% j+2 === 0){
+                if(i%j === 0 || i%(j+2) === 0){
                     return false;
                 }
-                j +=6;
+                j += 6;
                 console.log(i);
             }
         }
-
-printPrimes();
+    }
+ }
+    
+    printPrimes();
